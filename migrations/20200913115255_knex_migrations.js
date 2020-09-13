ce14d97d1b08530a8af59a44de9423291c635016
thejustvic <thejustvic@gmail.com>
2020-09-13 22:20:19 +0300

    changes in model

--- migrations/20200913115255_knex_migrations.js
+++ migrations/20200913115255_knex_migrations.js
@@ -1,24 +1,48 @@
 
+
+// Многие ко многим: hashtags <-> books
+// Многие ко многим: books <-> authors
+// Один ко многим: book -> characters
+// Один к одному: character <-> theCharacterHasSuperpowers
+
+
 exports.up = function(knex) {
   return knex.schema
+
+    
     .createTable('hashtags', table => {
+      table.increments('id').primary()
+      table.text('name', 50)
+        .notNullable(),
+      table.timestamps(true, true)
+    })
+
+    .createTable('books', table => {
       table.increments(), // 'id' field
       table.text('name', 50)
         .notNullable(),
       table.timestamps(true, true)
     })
-    .createTable('books', table => {
-      table.increments(), // 'id' field
+
+    // Многие ко многим: hashtags <-> books
+    .createTable('hashtags_books', table => {
+      table.integer('hashtag_id').unsigned().references('id').inTable('hashtags')
+      table.integer('book_id').unsigned().references('id').inTable('books')
+    })
+
+    // Один ко многим: book -> characters
+    .createTable('characters', table => {
+      table.increments('id').primary()
       table.text('name', 50)
         .notNullable()
-        .index(),
       table.timestamps(true, true)
 
-      // foreign key to 'hashtags'
-      table.integer('id_hashtag')
+      // Один ко многим: book -> characters
+      table.integer('book_id')
         .unsigned()
+        .notNullable()
         .references('id')
-        .inTable('hashtags')
+        .inTable('books')
         .onDelete('CASCADE')
         .onUpdate('CASCADE')
     })
@@ -27,5 +51,5 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return knex.schema
     .dropTableIfExists('books')
-    .dropTableIfExists('hashtags')
+    .dropTableIfExists('characters')
 };
