0cc70b2a408ae58c2dc42d967a1fd715a8935675
thejustvic <thejustvic@gmail.com>
2020-09-18 17:41:30 +0300

    typo indexjs

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
-const authRoutes = require('./routes/auth');
-const hashtagRoutes = require('./routes/hashtag');
+const authRoutes = require('./routes/auth')
+const hashtagRoutes = require('./routes/hashtag')
 
-app.use(cors());
-app.use(cookieParser());
+app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
 
 app.listen(process.env.PORT, () => console.log(`app is listening on ${process.env.PORT}`))
 
-app.use('/api', authRoutes);
+app.use('/api/auth', authRoutes)
 app.use('/api/hashtags', hashtagRoutes)
