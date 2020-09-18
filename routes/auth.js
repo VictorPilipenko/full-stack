869e844238f40465318d9737b0273fa6d9c35112
thejustvic <thejustvic@gmail.com>
2020-09-18 17:52:56 +0300

    auth routes refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
-const { Router } = require('express');
-const auth = require('../controllers/auth');
-const router = Router();
+const { Router } = require('express')
+const auth = require('../controllers/auth')
+const { createUser } = require('../validation/user')
+const router = Router()
 
-router.post('/register', auth.register);
-router.post('/login', auth.login);
-router.get('/logout', auth.logout);
-router.get('/logout', auth.logout);
-router.get('/check', auth.check);
+router.post('/register', createUser, auth.register)
+router.post('/login', auth.login)
+router.get('/logout', auth.logout)
+router.get('/check', auth.check)
 
-module.exports = router;
+module.exports = router
